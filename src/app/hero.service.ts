092d2bf5552108a4250bac1ecccb76e66914eac8
trapik@o2.pl <trapik@o2.pl>
2020-02-24 20:03:18 +0100

    Services added really

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
 
